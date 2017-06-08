19278ef1c89235a98a3c238744b7baf3062496be
Chris_Wallace <kawallis@icloud.com>
2017-06-08 09:58:11 -0700

    boiling hot

---
+++ todo/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+import {Provider} from 'react-redux';
+import{createStore} from 'redux';
+import reducers from './reducers';
+
+const store = createStore(reducers, {todo: []});
+
+ReactDOM.render(
+   < Provider store={store}>
+        <App />
+    </Provider>
+, document.getElementById('root'));
+registerServiceWorker();
